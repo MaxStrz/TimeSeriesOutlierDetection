fc2c893943dff3712e891db62097a5c22c2b88c6
MaxStrz <85751955+MaxStrz@users.noreply.github.com>
2024-11-07 14:39:25 +0100

    Updated basic testing of MedianLevelShift. Needs more testing.

--- test/MLS_test.py
+++ test/MLS_test.py
@@ -6,8 +6,10 @@ import seaborn as sns
 
 scaler = RobustScaler()
 
+# Generate univariate data from a Pareto distribution with shape/slope 3
+# and 1000 samples
 arr_pareto = np.random.pareto(3, (1000,))
 
-my_2sm = MLS()
-my_2sm.fit(arr_pareto)
+my_2sm = MLS() # Create a MedianLevelShift instance. Window size defaults to 50
+my_2sm.fit(arr_pareto) # Fit the model to the data
 print("MedianLevelShift tested successfully")
