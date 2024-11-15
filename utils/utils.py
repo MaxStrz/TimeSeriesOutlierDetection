d19192ec15472a799b55a31b2d6cd1a629f5fbe2
MaxStrz <85751955+MaxStrz@users.noreply.github.com>
2024-11-15 14:48:20 +0100

    Added remaining transformation methods. Added _called_methods attribute. Added run() method.

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,7 @@
+import numpy as np
 import os
 import pandas as pd
+from sklearn.preprocessing import RobustScaler
 
 class AnalysisPaths:
     def __init__(self, analysis_name: str):
@@ -40,9 +42,10 @@ class TransformRawData:
     # Name of the analysis must correspond to the folder name
     def __init__(self, analysis_name: str): 
         self.analysis_name = analysis_name
-        self.paths = AnalysisPaths(self.analysis_name)
+        self._paths = AnalysisPaths(self.analysis_name)
         self.labels = None
         self.df_all = None
+        self._called_methods = set()
 
     def _make_df(self,
                  file_path: str,
@@ -66,7 +69,7 @@ class TransformRawData:
     def create_df_all(self):
         dataframes = []
 
-        for file_path in self.paths.raw_file_paths:
+        for file_path in self._paths.raw_file_paths:
             # Generate dataframe from file. Expect message if successful.
             df_cs = self._make_df(file_path=file_path,
                           dtypes=self.config.dtypes,
@@ -80,6 +83,10 @@ class TransformRawData:
 
         self.df_all = pd.concat(dataframes, ignore_index=True)
 
+        self._called_methods.add("create_df_all")
+
+        return self
+
     def create_multi_index(self):
         df = self.df_all
         index = self.idx
@@ -88,3 +95,80 @@ class TransformRawData:
         df.index.names = index
 
         self.df_all = df
+
+        self._called_methods.add("create_multi_index")
+
+        return self
+
+    def _find_outliers(self, df):
+        mus = df.mean()
+        stds = df.std()
+        over_stds_idx = (np.abs(df - mus) > stds * 4).any(axis=1)
+        return over_stds_idx
+    
+    def clean_data(self):
+        cols_to_clean = self.sensors
+        df = self.df_all
+
+        # Only the kpis are required for removing outliers
+        df = df[cols_to_clean]
+        
+        # boolean indexes of outliers
+        over_stds_idx = self._find_outliers(df)
+
+        # remove outliers
+        # ~ ist a negation operator. It turns boolean values around.
+        self.df_all = self.df_all[~over_stds_idx].reset_index(drop=True)
+
+        self._called_methods.add("clean_data")
+
+        return self
+
+    def remove_columns(self):
+        df = self.df_all
+        cols_to_keep = self.sensors
+        df = df[cols_to_keep]
+        self.df_all = df
+
+        self._called_methods.add("remove_columns")
+
+        return self
+
+    def robust_scale_data(self):
+        
+        df = self.df_all
+        data = self.df_all.values
+
+        # Create and save scaler as attribute
+        scaler = RobustScaler().fit(data)
+
+        # Scale all data
+        scaled_data = scaler.transform(data)
+
+        # Create new dataframe with scaled data
+        self.df_all = pd.DataFrame(scaled_data,
+                                   columns=df.columns,
+                                   index=df.index)
+        
+        # Save scaler as attribute
+        self.robust_scaler = scaler
+
+        self._called_methods.add("robust_scale_data")
+
+        return self
+
+    def run(self):
+        return(
+            self.create_df_all()
+            .create_multi_index()
+            .clean_data()
+            .remove_columns()
+            .robust_scale_data()
+        )
+
+    def __repr__(self):
+        methods = {"create_df_all", "create_multi_index", "clean_data",
+                   "remove_columns", "robust_scale_data", "None"}
+        pending_methods = methods - self._called_methods
+        note = f"TransformRawData instance. Pending methods: {pending_methods}"
+        return note
